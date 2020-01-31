7626843675beaa3e09f0e48c1b1010995eb41289
qiangyongjun <qiangyongjun918@163.com>
2020-01-31 13:42:50 +0800

    controlpanel

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import Antd from 'ant-design-vue';
 import Button from 'ant-design-vue/lib/button';
 import 'ant-design-vue/dist/antd.css';
 import App from './App.vue'
 
 Vue.component(Button.name, Button);
 Vue.config.productionTip = false
+Vue.use(Antd);
 
 new Vue({
   render: h => h(App),
